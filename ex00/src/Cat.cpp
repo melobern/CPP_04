ba114195a991fbdf9a677bc4ed0a5ea9e822d5cc
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-08-22 14:59:53 +0200

    ex00 finished, maybe check some things later

--- ex00/src/Cat.cpp
+++ ex00/src/Cat.cpp
@@ -6,34 +6,35 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 11:24:03 by mbernard          #+#    #+#             */
-/*   Updated: 2024/08/22 11:30:09 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/08/22 14:48:54 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Cat.hpp"
 
-Cat::Cat(void) {
-    std::cout << "Cat :";
+Cat::Cat(void) : Animal("Cat") {
+    std::cout << "Cat\t\t: ";
     std::cout << "Default constructor called" << std::endl;
     return;
 }
 
 Cat::Cat(const Cat &cat) {
-    std::cout << "Cat : ";
+    std::cout << "Cat\t\t: ";
     std::cout << "Copy constructor called" << std::endl;
     *this = cat;
     return;
 }
 
 Cat::~Cat() {
-    std::cout << "Cat : ";
-    std::cout <<"Destructor called" << std::endl;
+    std::cout << "Cat\t\t: ";
+    std::cout << "Destructor called" << std::endl;
     return;
 }
 
 Cat   &Cat::operator=(const Cat &cat) {
-    this->Cat::~Cat();
-    new (this) Cat(cat);
+    if (this != &cat) {
+        this->type = cat.type;
+    }
     std::cout << "Copy assignment operator called" << std::endl;
     return (*this);
 }
--- ex00/src/WrongAnimal.cpp
+++ ex00/src/WrongAnimal.cpp
@@ -6,36 +6,39 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 10:15:55 by mbernard          #+#    #+#             */
-/*   Updated: 2024/08/22 11:25:44 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/08/22 14:50:41 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/WrongAnimal.hpp"
 
-WrongAnimal::WrongAnimal(void): type("unknown") {
+WrongAnimal::WrongAnimal(void): type("Unknown") {
+    std::cout << "WrongAnimal\t: ";
     std::cout << "Default constructor called" << std::endl;
     return;
 }
 
 WrongAnimal::WrongAnimal(const std::string type): type(type) {
-    std::cout << "WrongAnimal " << type;
-    std::cout << " : constructor called" << std::endl;
+    std::cout << "WrongAnimal\t: ";
+    std::cout << "Constructor called" << std::endl;
     return;
 }
 
 WrongAnimal::WrongAnimal(const WrongAnimal &animal) {
+    std::cout << "WrongAnimal\t: ";
     std::cout << "Copy constructor called" << std::endl;
     *this = animal;
     return;
 }
 
 WrongAnimal::~WrongAnimal() {
-    std::cout << "WrongAnimal " << this->type;
-    std::cout <<" : Destructor called" << std::endl;
+    std::cout << "WrongAnimal\t: " << this->type;
+    std::cout << " Destructor called" << std::endl;
     return;
 }
 
 WrongAnimal   &WrongAnimal::operator=(const WrongAnimal &animal) {
+    std::cout << "WrongAnimal\t: ";
     std::cout << "Copy assignment operator called" << std::endl;
     if (this != &animal) {
         this->type = animal.type;
@@ -48,7 +51,7 @@ std::string    WrongAnimal::getType() const {
 }
 
 void    WrongAnimal::makeSound() const {
-    std::cout << "awwouuuwarrwrrr : Strange unidentified noise.";
+    std::cout << "awwouuuwarrwrrr : Strange unidentified noise. ";
     std::cout << "This sound seems so wrong." << std::endl;
 }
 
